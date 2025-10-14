bb198f16a7a8bedec3509b78219c91de49e6cf97
Omar Pachas <jesuspachas0403@gmail.com>
2025-10-14 15:44:52 -0500

    Archivo pruebas implementado

--- config/database.php
+++ config/database.php
@@ -1,11 +1,11 @@
 <?php
 
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'marketperu.database.windows.net');
 define('DB_NAME', 'supermercado');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_USER', 'adminsql');
+define('DB_PASS', 'Sql1908.');
 define('DB_CHARSET', 'utf8mb4');
 
 define('APP_NAME', 'Supermercado - Gestión de Productos');
-define('BASE_URL', 'http://localhost/supermercado/');
+define('BASE_URL', 'http://localhost/supermerado/');
 
--- config/prueba.php
+++ config/prueba.php
@@ -11,4 +11,3 @@ try {
 } catch (PDOException $e) {
     echo "❌ Error al conectar: " . $e->getMessage();
 }
-
