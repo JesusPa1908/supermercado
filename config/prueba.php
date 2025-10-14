15b68d09d31e775f9b452e224cbbc565d3c6e54f
Jesús Pachas <146782797+JesusPa1908@users.noreply.github.com>
2025-10-14 18:03:10 -0500

    Update prueba.php

--- config/prueba.php
+++ config/prueba.php
@@ -1,13 +1,12 @@
 <?php
 $host = "marketperu.database.windows.net";  // servidor SQL
 $dbname = "supermercado";                   // base de datos
-$user = "adminsql";              // tu usuario administrador
-$pass = "Sql1908.";                 // tu contraseña
-
+$user = "adminsql";              
+$pass = "Sql1908.";                
 try {
     $conn = new PDO("sqlsrv:server=$host;Database=$dbname", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "✅ Conexión exitosa a SQL Server en Azure!";
+    echo "Conexión exitosa a SQL Server en Azure!";
 } catch (PDOException $e) {
-    echo "❌ Error al conectar: " . $e->getMessage();
+    echo "Error al conectar: " . $e->getMessage();
 }
